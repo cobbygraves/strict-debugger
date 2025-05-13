0255eafd77d3995d287e5b8c5012b3e0e86863ab
cobbygraves <rmeocnis@hotmail.com>
2025-05-13 22:12:44 +0000

    removed chat.js import from advanceSettings

--- build/js/main.js
+++ build/js/main.js
@@ -9,8 +9,8 @@ const advanceFeaturesContainer = document.querySelector(
 const nav = document.querySelector('nav')
 const loader = document.querySelector('.loader-container')
 // imports
-import Light from './basicSettings'
-import AdvanceSettings from './advanceSettings'
+import Light from '../js/basicSettings.js'
+import AdvanceSettings from '../js/advanceSettings.js'
 // object creation
 const lightController = new Light()
 const advancedSettings = new AdvanceSettings()
